19b3966d41207e5e586c79cd1415375170ecf162
JohannesHoppe <johannes.hoppe@haushoppe-its.de>
2014-07-15 08:42:19 +0200

    crawler finished

---
+++ TalkDataCrawler/TalkDataCrawler/dwxCrawler.js
@@ -0,0 +1,53 @@
+﻿var util = require('util'),
+    extend = require('extend'),
+    events = require('events'),
+    cheerio = require('cheerio'),
+    SimpleCrawler = require("simplecrawler");
+
+function Crawler(config) {
+
+    this.config = config;
+    this.talks = [];
+
+    this._crawler = new SimpleCrawler(this.config.host);
+    extend(this._crawler, config);
+    
+    this._crawler.on("fetchcomplete", this._fetchcomplete.bind(this));
+    this._crawler.on("complete", function () {
+        this.emit('complete', this.talks);
+    }.bind(this));
+
+}
+
+util.inherits(Crawler, events.EventEmitter);
+
+
+Crawler.prototype._fetchcomplete = function(queueItem , responseBuffer , response) {
+
+    console.log("Completed fetching resource:", queueItem.url);
+
+    var html = responseBuffer.toString();
+    var $ = cheerio.load(html);
+
+    var title = $(".container h2").first().text().trim();
+    var description = $(".container p").first().text().trim();
+    var time_and_track = $(".container .ezagenda_date").first().text().split("Track:");     
+    var speaker = $("h4").next().find("a").first().text().trim();
+
+    if (!title) {
+        return;
+    }
+
+    this.talks.push({
+        title: title,
+        description: description,
+        time_and_track: time_and_track,
+        speaker: speaker
+    });
+};
+
+Crawler.prototype.start = function() {
+    this._crawler.start();
+};
+
+module.exports = Crawler;
--- TalkDataCrawler/TalkDataCrawler/start.js
+++ TalkDataCrawler/TalkDataCrawler/start.js
@@ -1,1 +1,32 @@
-﻿console.log('Hello world');
+﻿/**
+ * A simple crawler to get the events from DWX 2014
+ */
+
+var fs = require('fs'),
+    DwxCrawler = require('./dwxCrawler'),
+    dwxCrawler,
+
+    crawlerConfig = {
+        host: 'www.developer-week.de',
+        initialPath: '/Programm',
+        discoverRegex: [
+            /(\shref\s?=\s?)['"](\/Programm\/Veranstaltung\/\(event\)\/[^"']+)/ig
+        ],
+        userAgent: 'DWX 2014 TalkDataCrawler (by Johannes Hoppe)'
+    },
+    fileNameJsonP = '../talks_callback.json';
+
+var saveStringify = function (obj) {
+    return JSON.stringify(obj)
+        .replace(/\u2028/g, '\\u2028')
+        .replace(/\u2029/g, '\\u??2029');
+}
+
+dwxCrawler = new DwxCrawler(crawlerConfig);
+dwxCrawler.on('complete', function (talks) {
+
+    var content = saveStringify(talks);
+    fs.writeFile(fileNameJsonP, content, 'utf8', console.log);
+});
+
+dwxCrawler.start();
